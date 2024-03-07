6ed347084175f6540bf692db5e2b2ab12d89baf3
babul101 <babuldeb101@gmail.com>
2024-03-07 19:13:34 +0530

    making the initial type to it and see how it changes from previous to current style

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,22 @@ import {motion} from 'framer-motion'
 
 const App = () => {
   return (
-    <div className='flex items-center justify-center'>
+    <div style={{
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh', 
+      width: '100vw',
+     }}>
       <motion.button className='bg-black text-white px-3 py-2 rounded-md' animate={{
         backgroundColor:'#f00',
         color:'white',
         scale:2,
-        y:50,
-        rotate:1800
-      }}>Framer Motion</motion.button>
+        
+      }}
+      initial={{background:'violet'}}
+      
+      >Framer Motion</motion.button>
     </div>
   )
 }
